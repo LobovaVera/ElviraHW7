02f69d071a60e3ac2efbe0c6d293d77ad9c69f94
Vera Lobova <pchilli@yandex.ru>
2022-10-25 16:05:59 +0300

    task 3 corrected

--- TASK3/Program.cs
+++ TASK3/Program.cs
@@ -55,10 +55,10 @@ void PrintArray(int[,] array)
 void EachRowArithmeticMean(int[,] array)
 {    double arithmeticMean = 0;
   
-    for(int i = 0; i<array.GetLength(0); i++)
+    for(int i = 0; i<array.GetLength(1); i++)
     {
          arithmeticMean = 0;
-        for(int j =0; j<array.GetLength(1); j++)
+        for(int j =0; j<array.GetLength(0); j++)
         {
             
             arithmeticMean += Convert.ToDouble(array[j,i]);
